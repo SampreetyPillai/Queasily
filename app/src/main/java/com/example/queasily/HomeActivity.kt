116e2a1c69bb50b49b18942bf04b9ba58e5f3935
Sampreety Pillai <p.sampreety@gmail.com>
2022-12-08 13:24:17 +0530

    Uploading again

--- app/src/main/java/com/example/queasily/HomeActivity.kt
+++ app/src/main/java/com/example/queasily/HomeActivity.kt
@@ -1,14 +1,34 @@
 package com.example.queasily
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 import java.time.LocalDateTime
 
 class HomeActivity : AppCompatActivity() {
+
+    public var ongoing:String = "dashboard"
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +37,76 @@ class HomeActivity : AppCompatActivity() {
         val myIntent = getIntent()
         val USERNAME:String? = myIntent.getStringExtra("USERNAME")
 
-        findViewById<Button>(R.id.attempt_quiz).setOnClickListener{
-            val newIntent = Intent(this, Questions::class.java)
-            newIntent.putExtra("Time", LocalDateTime.now())
-            newIntent.putExtra("qno",1)
-            newIntent.putExtra("USERNAME",USERNAME)
-            newIntent.putExtra("quizid","tdF963EsyAtkY5ifObtI")
-            startActivity(newIntent)
-        }
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+        toolbar = findViewById(R.id.mytoolbar)
+
+//        setSupportActionBar(toolbar)
+//        val toggle:ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.OpenDrawer, R.string.CloseDrawer)
+//
+//        drawerLayout.addDrawerListener(toggle)
+//        toggle.syncState()
+
+        val data = Bundle()
+        data.putString("USERNAME", USERNAME)
+        Log.d(TAG, "this is the username ${USERNAME}")
+//        val mfrag:Fragment = dashboardFragment()
+//        mfrag.arguments = data
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val mfrag:Fragment = dashboardFragment()
+        mfrag.arguments = data
+        fragmentTransaction.add(R.id.container, mfrag)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+
+       navigationView.setNavigationItemSelectedListener{item->
+
+           val data:Bundle?=null
+           data?.putString("USERNAME", USERNAME)
+           var myfragment:Fragment?=null
+           myfragment?.arguments = data
+
+           val id:Int  = item.itemId
+
+           when (id){
+               R.id.upcoming_menu -> {
+                   Toast.makeText(this, "Upcoming Quizes", Toast.LENGTH_SHORT).show()
+                   myfragment = dashboardFragment()
+
+               }
+               R.id.past_menu -> {
+                   Toast.makeText(this, "Past quizes", Toast.LENGTH_SHORT).show()
+                   myfragment = dashboardFragment()
+               }
+               R.id.missed_menu -> {
+                   Toast.makeText(this, "Missed Quizzes", Toast.LENGTH_SHORT).show()
+                   myfragment = dashboardFragment()
+               }
+               R.id.dashboard_menu -> {
+                   Toast.makeText(this, "Your Dashboard", Toast.LENGTH_SHORT).show()
+                   myfragment = dashboardFragment()
+               }
+
+           }
+//           if (myfragment != null) {
+//               loadFragment(myfragment)
+//           }
+
+           drawerLayout.closeDrawer(GravityCompat.START)
+           true
+
+
+       }
+
+
     }
+
+
+//    override fun OnNavigationItemSelected(item: MenuItem):Boolean{
+//
+//
+//    }
+
+
 }
---
+++ app/src/main/java/com/example/queasily/dashboardFragment.kt
@@ -0,0 +1,75 @@
+package com.example.queasily
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import java.util.ResourceBundle.getBundle
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [dashboardFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class dashboardFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//
+
+        val USERNAME = arguments?.getString(ARG_NAME)
+
+        Log.d(TAG, "the username is ${USERNAME}")
+
+        var view:View = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+        view.findViewById<TextView>(R.id.sample).text = USERNAME.toString()
+        return view
+    }
+
+    companion object {
+        const val ARG_NAME = "USERNAME"
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment dashboardFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param0:String,param1: String, param2: String) =
+            dashboardFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                    putString(ARG_NAME, param0)
+                }
+
+            }
+    }
+}
